829f80c5ec3ff769bd90f2a179c64f1178c1e7fe
Mayur Adhude | मयूर आधुडे <mayur.adhude1998@gmail.com>
2022-10-30 23:33:31 +0530

    Game of "Functions"

---
+++ game_of_functions.py
@@ -0,0 +1,28 @@
+# Assignment-3: Functions | Modules
+
+# Game of "Functions"
+
+# Write a Python function to sum all the numbers in a list.
+
+# Sample List : (8, 2, 3, 0, 7)
+
+# Expected Output : 20
+
+# Explanation:
+
+# Summation should like 8+2+3+0+7 = 20
+
+# Should have used function to implement the code - 15
+
+# Must print the Expected output - 15
+
+
+def sum(list):
+    sum1= 0
+
+    for i in list:
+        sum1+= i
+    return sum1
+
+list=[8, 2, 3, 0, 7]
+print(sum(list))
